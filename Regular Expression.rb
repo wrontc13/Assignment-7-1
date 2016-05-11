6216ca22a3989aa304c0aaf5b11ec89610ce86dd
wrontc13 <wrontc13@gmail.com>
2016-05-10 20:31:57 -0400

    Add files via upload

---
+++ Regular Expression.rb
@@ -0,0 +1,12 @@
+email = "John@gmail.com"
+if (email =~ /gmail.com(.*)/)
+puts "gmail located"
+end
+document = "asarfre.rb"
+if (document =~ /.rb(.*)/)
+puts "Ruby file Located"
+end
+pictures = "flower.png"
+if (pictures =~ /.png(.*)/)
+puts "PNG located"
+end
